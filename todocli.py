9499922b878638cad4ea4b3a7b8a336ef493c4a8
LukasDrsman <lukaskodr@gmail.com>
2020-04-14 09:29:10 +0200

    Clear terminal on exit

--- todocli.py
+++ todocli.py
@@ -16,14 +16,19 @@ while (1):
         parameter = ""
     if (command == "new" or command == "n"):
         new(parameter)
+        show()
     if (command == "check" or command == "cc"):
         check(parameter)
+        show()
     if (command == "uncheck" or command == "uc"):
         uncheck(parameter)
+        show()
     if (command == "quit" or command == "q"):
+        print("\033c", end="")
         exit()
     if (command == "exit" or command == "x"):
+        print("\033c", end="")
         exit()
     if (command == "clear" or command == "l"):
         delete()
-    show()
+        show()
