6a5755f2bbae196cd021da105d77f2beb02f9c49
LukasDrsman <lukaskodr@gmail.com>
2020-06-03 18:52:41 +0200

    Fixed install path

--- functions.py
+++ functions.py
@@ -4,13 +4,13 @@ from os.path import expanduser, isfile
 from datetime import datetime
 
 # TODO: task descriptions
-# TODO: sort:
-# TODO:     smartsort
 
+# todolist structure: [[task text, flag, duedate], [task text, flag, duedate]]
+
+# fetch current date
 todate = datetime.today()
-path = os.path.expanduser(defpath)
-ddir = os.path.expanduser(defdir)
 
+# change flag of task | args: (todolist, flag command, number of task)
 def change_flag(todo, command, n):
     try:
         tasknum = int(n) - 1
@@ -23,22 +23,24 @@ def change_flag(todo, command, n):
             except:
                 pass
 
+# load todolist | args: (path to todolist) | returns: todolist, todolist title
 def load(path):
-    list = []
+    todo = []
     file = open(path, 'r+')
     pretitle, mess = str(os.path.basename(file.name)).split(".")
     title = pretitle+": "
     lines = file.readlines()
     for i in range(len(lines)):
         try:
-            task, state, duedate = (lines[i].rstrip("\n")).split("|")
+            task, flag, duedate = (lines[i].rstrip("\n")).split("|")
             due = datetime.strptime(duedate, '%Y-%m-%d')
-            list.append([task, state, due])
+            todo.append([task, flag, due])
         except:
-            task, state  = (lines[i].rstrip("\n")).split("|")
-            list.append([task, state, "none"])
-    return list, title
+            task, flag  = (lines[i].rstrip("\n")).split("|")
+            todo.append([task, flag, "none"])
+    return todo, title
 
+# render todolist | args: (todolist, todolist title)
 def show(todo, title):
     print("\033c", end="")
     if(showtitle == True):
@@ -49,30 +51,31 @@ def show(todo, title):
             print(linenumformat % num, end="")
         for y in range(len(flags)):
             if(flags[y][1][1] == todo[i][1]):
-                taskinfo = flags[y][1][0]+" "+todo[i][0]
+                taskinfo = flags[y][1][0] + " " + todo[i][0]
                 if(showdeadline == False):
                     if(type(todo[i][2]) is datetime):
                         if(todo[i][2] >= todate):
-                            print(flags[y][2][0]+taskinfo+END)
+                            print(flags[y][2][0] + taskinfo + END)
                         elif(todo[i][2] < todate and flags[y][2][1] == True):
-                            print(flags[y][2][0]+taskinfo+END)
+                            print(flags[y][2][0] + taskinfo + END)
                         else:
-                            print(RED+taskinfo+END)
+                            print(RED + taskinfo + END)
                     else:
-                        print(flags[y][2][0]+taskinfo+END)
+                        print(flags[y][2][0] + taskinfo + END)
                 if(showdeadline == True):
                     if(type(todo[i][2]) is datetime):
                         if(todo[i][2] >= todate):
-                            print(flags[y][2][0]+taskinfo+END+" "+" → "+todo[i][2].strftime(dateformat))
+                            print(flags[y][2][0] + taskinfo + END + " " + " → " + todo[i][2].strftime(dateformat))
                         elif(todo[i][2] < todate and flags[y][2][1] == True):
-                            print(flags[y][2][0]+taskinfo+END+" "+" → "+todo[i][2].strftime(dateformat))
+                            print(flags[y][2][0] + taskinfo + END + " " + " → " + todo[i][2].strftime(dateformat))
                         else:
-                            print(RED+taskinfo+END+" "+" → "+todo[i][2].strftime(dateformat))
+                            print(RED + taskinfo + END + " " + " → " + todo[i][2].strftime(dateformat))
                     else:
-                        print(flags[y][2][0]+taskinfo+END)
+                        print(flags[y][2][0] + taskinfo + END)
                 break
     print(" ")
 
+# save todolist | args: (todolist, path to todolist file)
 def save(todo, path):
     file = open(path, 'w')
     file.write("")
@@ -80,13 +83,15 @@ def save(todo, path):
     file = open(path, 'a')
     for i in range(len(todo)):
         if(type(todo[i][2]) is datetime):
-            file.write(todo[i][0]+"|"+todo[i][1]+"|"+todo[i][2].strftime('%Y-%m-%d')+"\n")
+            file.write(todo[i][0] + "|" + todo[i][1] + "|" + todo[i][2].strftime('%Y-%m-%d') + "\n")
         else:
-            file.write(todo[i][0]+"|"+todo[i][1]+"\n")
+            file.write(todo[i][0] + "|" + todo[i][1] + "\n")
 
+# create new task | args: (todolist, text)
 def new(todo, task):
     todo.append([task, deflag, "none"])
 
+# change duedate of task | args: (todolist, number of task)
 def change_date(todo, n):
         try:
             tasknum = int(n) - 1
@@ -94,9 +99,9 @@ def change_date(todo, n):
             tasknum = len(todo) + 1
 
         if(thisyear == False):
-            duedate = input("(Year month day) "+promptcol+prompt+END)
+            duedate = input("(Year month day) " + promptcol + prompt + END)
         else:
-            duedate = input("(month day) "+promptcol+prompt+END)
+            duedate = input("(month day) " + promptcol + prompt + END)
         try:
             if(thisyear == False):
                 due = datetime.strptime(duedate, '%Y %m %d')
@@ -110,6 +115,7 @@ def change_date(todo, n):
         except:
             pass
 
+# remove single task | args: (todolist, number of task)
 def remove(todo, n):
         try:
             tasknum = int(n) - 1
@@ -120,15 +126,21 @@ def remove(todo, n):
         except:
             pass
 
+# remove all tasks | args: (todolist)
 def clear(todo):
-    consent = input("Are you sure you want to clear your todolist?\n([y]es or [n]o) "+promptcol+prompt+END)
+    consent = input("Are you sure you want to clear your todolist?\n([y]es or [n]o) " + promptcol + prompt + END)
     consent.lower()
     if(consent == "y" or consent == "yes"):
         todo.clear()
 
+# sort todolist | args: (todolist, sorting method, sort direction)
+# sort_type: date/d, priority/p
+# direction: highest/h, lowest/l
 def sort_by(todo, sort_type, direction):
     sort_type.lower()
     direction.lower()
+
+    # sort todolist by duedates assigned to tasks
     if(sort_type == "date" or sort_type == "d"):
         wdate = []
         wodate = []
@@ -146,7 +158,8 @@ def sort_by(todo, sort_type, direction):
             todo.append(wdate[i])
         for i in range(len(wodate)):
             todo.append(wodate[i])
-
+    
+    # sort todolist by priority value of flags assigned to tasks
     if(sort_type == "priority" or sort_type == "p"):
         wpriority = []
         for i in range(len(todo)):
